025e433734a35caf3aec2387411bd0d951174f27
Nurcan Cürebal <nurcancurebal@hotmail.com>
2023-11-20 23:19:49 +0300

    .

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+
+  <!-- Your appBar goes here -->
+  <v-app-bar  title="My site" app>
+  </v-app-bar>  
+
+</v-app>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
