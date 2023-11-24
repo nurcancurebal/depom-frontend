a5c5efd6b1bfee2928f93dbba57548ac0c0efeb4
Nurcan Cürebal <nurcancurebal@hotmail.com>
2023-11-24 17:56:02 +0300

    navbar

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,16 +1,51 @@
 <template>
-  <v-app>
-
-  <!-- Your appBar goes here -->
-  <v-app-bar  title="My site" app>
-  </v-app-bar>  
-
-</v-app>
+  <v-row no-gutters>
+    <v-col cols="12">
+      <v-sheet>
+        <v-toolbar style="color: white; background-color: #2d98c9">
+          <v-toolbar-title class="text-h4"> Stok Takip </v-toolbar-title>
+        </v-toolbar>
+      </v-sheet>
+    </v-col>
+    <v-col cols="4">
+      <v-card style="border-radius: unset">
+        <v-layout>
+          <v-navigation-drawer
+            floating
+            permanent
+            style="position: absolute; width: 100%; background-color: #ededed"
+          >
+            <v-list density="compact" nav>
+              <v-list-item
+                prepend-icon="mdi-view-dashboard"
+                title="Home"
+                value="home"
+              />
+              <v-list-item
+                prepend-icon="mdi-forum"
+                title="About"
+                value="about"
+              />
+            </v-list>
+          </v-navigation-drawer>
+          <v-main style="height: 250px" />
+        </v-layout>
+      </v-card>
+    </v-col>
+    <v-col cols="8">
+      <v-sheet>
+        <v-toolbar style="color: white; background-color: #2d98c9">
+          <v-toolbar-title class="text-h4"> Stok Takip </v-toolbar-title>
+        </v-toolbar>
+      </v-sheet>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {};
+  },
+};
 </script>
-
-<style>
-</style>
