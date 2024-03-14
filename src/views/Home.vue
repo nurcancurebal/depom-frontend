bf1b2ab11bdce34b85f962d9c6ad32c035a5fba1
Nurcan <nurcancurebal@hotmail.com>
2024-03-14 11:18:27 +0300

    .

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,18 +8,15 @@
       <v-sheet
         :elevation="13"
         :height="400"
-        style="border-radius: 25px 0px 0px 25px"
+        style="border-radius: 25px 0px 0px 25px; background-color: #208ec6"
         class="text-center py-15"
       >
-        <h2 class="text-h3 font-weight-bold" style="color: #208ec6">Depom</h2>
-        <p class="my-4 text-medium-emphasis text-body-1 text-center">
+        <h2 class="text-h3 font-weight-bold" style="color: #ededed">Depom</h2>
+        <p class="my-4 text-body-1 text-center" style="color: #ededed">
           Stok takip ve yönetim uygulaması
         </p>
-        <div
-          style="text-align: left; display: inline-block"
-          class="text-medium-emphasis"
-        >
-          <ul style="list-style-type: disc">
+        <div style="text-align: left; display: inline-block">
+          <ul style="list-style-type: disc; color: #ededed">
             <li>Depo - Stok</li>
             <li>Stok Giriş</li>
             <li>Stok Çıkış</li>
@@ -33,14 +30,41 @@
         :elevation="13"
         :height="400"
         style="border-radius: 0px 25px 25px 0px"
-      ></v-sheet>
+      >
+        <form class="d-flex align-center flex-column justify-center pt-16">
+          <v-text-field
+            prepend-inner-icon="mdi-account"
+            variant="solo"
+            rounded="lg"
+            style="width: 300px"
+          />
+          <v-text-field
+            prepend-inner-icon="mdi-lock"
+            variant="solo"
+            style="width: 300px"
+          />
+          <v-btn
+            class="mt-2"
+            type="submit"
+            style="
+              color: #757575;
+              width: 300px;
+              background-color: #208ec6;
+              color: #ededed;
+            "
+          >
+            Giriş Yap
+          </v-btn>
+        </form>
+      </v-sheet>
     </v-col>
   </v-row>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {};
+  },
+};
 </script>
-
-<style>
-</style>
