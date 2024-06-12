fad6ed378ad39c2f1024c0575a05898450582da7
nurcan <nurcancurebal@hotmail.com>
2024-06-12 16:04:13 +0300

    theappbar components

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      v-if="$route.name !== 'UserAuth'"
-      style="background-color: #208ec6"
-      class="text-white"
-      prominent
-    >
-      <img
-        width="50"
-        height="50"
-        aspect-ratio="16/9"
-        cover
-        src="@/assets/logo.png"
-        alt="logo"
-        class="ml-4"
-      />
-      <v-toolbar-title class="text-h4">
-        <router-link
-          to="/stock"
-          id="title"
-          class="text-white text-decoration-none"
-        >
-          Depom
-        </router-link>
-      </v-toolbar-title>
-
-      <template v-if="$vuetify.display.lgAndUp">
-        <v-toolbar-items class="align-center">
-          {{ formattedName(userData.firstname) }}
-          {{ formattedName(userData.lastname) }}
-        </v-toolbar-items>
-
-        <v-btn icon>
-          <v-icon icon="mdi-account"></v-icon>
-          <v-menu activator="parent">
-            <v-list>
-              <router-link
-                style="color: #208ec6; font-family: 'Material Design Icons'"
-                to="/update-account"
-                class="text-decoration-none"
-              >
-                <v-list-item-title value="hesabimiduzenle" class="pa-1 mx-2">
-                  Hesabımı Düzenle
-                </v-list-item-title>
-              </router-link>
-
-              <router-link
-                style="color: #208ec6; font-family: 'Material Design Icons'"
-                to="/"
-                class="text-decoration-none"
-                @click="exitToast"
-              >
-                <v-list-item-title value="cikis" class="pa-1 mx-2">
-                  Çıkış
-                </v-list-item-title>
-              </router-link>
-            </v-list>
-          </v-menu>
-        </v-btn>
-      </template>
-      <v-btn
-        v-if="$vuetify.display.mdAndDown"
-        icon="mdi-dots-vertical"
-        @click.stop="drawer = !drawer"
-      />
-    </v-app-bar>
+    <TheAppbar @update:drawer="handleDrawerUpdate" />
 
     <TheNavbar :drawer="drawer" />
 
@@ -75,9 +11,8 @@
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex";
-import { useToast } from "vue-toast-notification";
 import TheNavbar from "./components/TheNavbar.vue";
+import TheAppbar from "./components/TheAppbar.vue";
 
 export default {
   data() {
@@ -88,32 +23,19 @@ export default {
 
   components: {
     TheNavbar,
-  },
-
-  computed: {
-    ...mapGetters("user", { userData: "user" }),
+    TheAppbar,
   },
 
   created() {
-    this.getUser();
     this.handleResize();
     window.addEventListener("resize", this.handleResize);
   },
 
   methods: {
-    ...mapActions("user", ["getUser"]),
-    exitToast() {
-      const toast = useToast();
+    handleDrawerUpdate(newDrawerValue) {
+      this.drawer = newDrawerValue;
+    },
 
-      toast.success("Başarıyla çıkış yaptınız.", {
-        position: "bottom",
-        duration: 2000,
-      });
-    },
-    formattedName(name) {
-      if (!name) return "";
-      return name.charAt(0).toUpperCase() + name.slice(1);
-    },
     handleResize() {
       if (window.innerWidth > 1280) {
         this.drawer = true;
@@ -132,9 +54,6 @@ export default {
 }
 a:hover {
   background-color: #fdfcfcac;
-}
-#title:hover {
-  background-color: #208ec6;
 }
 .router-link {
   text-decoration: none;
---
+++ src/components/TheAppbar.vue
@@ -0,0 +1,117 @@
+<template>
+  <v-app-bar
+    v-if="$route.name !== 'UserAuth'"
+    style="background-color: #208ec6"
+    class="text-white"
+    prominent
+  >
+    <img
+      width="50"
+      height="50"
+      aspect-ratio="16/9"
+      cover
+      src="@/assets/logo.png"
+      alt="logo"
+      class="ml-4"
+    />
+    <v-toolbar-title class="text-h4">
+      <router-link
+        to="/stock"
+        id="title"
+        class="text-white text-decoration-none"
+      >
+        Depom
+      </router-link>
+    </v-toolbar-title>
+
+    <template v-if="$vuetify.display.lgAndUp">
+      <v-toolbar-items class="align-center">
+        {{ formattedName(userData.firstname) }}
+        {{ formattedName(userData.lastname) }}
+      </v-toolbar-items>
+
+      <v-btn icon>
+        <v-icon icon="mdi-account"></v-icon>
+        <v-menu activator="parent">
+          <v-list>
+            <router-link
+              style="color: #208ec6; font-family: 'Material Design Icons'"
+              to="/update-account"
+              class="text-decoration-none"
+            >
+              <v-list-item-title value="hesabimiduzenle" class="pa-1 mx-2">
+                Hesabımı Düzenle
+              </v-list-item-title>
+            </router-link>
+
+            <router-link
+              style="color: #208ec6; font-family: 'Material Design Icons'"
+              to="/"
+              class="text-decoration-none"
+              @click="exitToast"
+            >
+              <v-list-item-title value="cikis" class="pa-1 mx-2">
+                Çıkış
+              </v-list-item-title>
+            </router-link>
+          </v-list>
+        </v-menu>
+      </v-btn>
+    </template>
+    <v-btn
+      v-if="$vuetify.display.mdAndDown"
+      icon="mdi-dots-vertical"
+      @click.stop="emitDraver"
+    />
+  </v-app-bar>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+import { useToast } from "vue-toast-notification";
+
+export default {
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+
+  computed: {
+    ...mapGetters("user", { userData: "user" }),
+  },
+
+  created() {
+    this.getUser();
+  },
+
+  methods: {
+    ...mapActions("user", ["getUser"]),
+
+    emitDraver() {
+      this.drawer = !this.drawer;
+      this.$emit("update:drawer", this.drawer);
+    },
+
+    formattedName(name) {
+      if (!name) return "";
+      return name.charAt(0).toUpperCase() + name.slice(1);
+    },
+
+    exitToast() {
+      const toast = useToast();
+
+      toast.success("Başarıyla çıkış yaptınız.", {
+        position: "bottom",
+        duration: 2000,
+      });
+    },
+  },
+};
+</script>
+
+<style>
+#title:hover {
+  background-color: #208ec6;
+}
+</style>
